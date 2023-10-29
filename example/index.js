b15bf0f9b9b0a0ffa35d31eb5296bbf34a24a023
jmRakoto <jannymichelr@gmail.com>
2023-10-29 21:09:49 +0300

    remove codecov

---
+++ example/index.js
@@ -0,0 +1,12 @@
+module.exports.handler = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: "AWS Serverless x fluentci",
+      },
+      null,
+      2
+    ),
+  };
+};
