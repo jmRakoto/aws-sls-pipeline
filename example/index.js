51c78930f016e5f457f0604c74064d67118a233b
jmRakoto <jannymichelr@gmail.com>
2023-10-30 10:32:31 +0300

    update action

--- example/index.js
+++ example/index.js
@@ -3,7 +3,7 @@ module.exports.handler = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "AWS Serverless x fluentci test",
+        message: "AWS Serverless x fluentci",
       },
       null,
       2
