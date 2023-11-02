ea26ad5e7c60dfbae2e12939db1cd1520b0d2fbb
jmRakoto <jannymichelr@gmail.com>
2023-11-02 21:12:00 +0300

    fix bucket

--- example/index.js
+++ example/index.js
@@ -3,7 +3,7 @@ module.exports.handler = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "AWS Serverless x fluentci 🚀",
+        message: "AWS Serverless x fluentci",
       },
       null,
       2
