296b60c0377f406e289c6847ebc9fba6548d78e4
jmRakoto <jannymichelr@gmail.com>
2023-11-02 21:02:03 +0300

    test new env

--- example/index.js
+++ example/index.js
@@ -3,7 +3,7 @@ module.exports.handler = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "AWS Serverless x fluentci",
+        message: "AWS Serverless x fluentci 🚀",
       },
       null,
       2
